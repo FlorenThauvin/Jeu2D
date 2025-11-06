78024e092aefd7d1cb24cd0b8d2d836d3db45fec
Florent EDMOND <florent_1999@hotmail.fr>
2025-11-06 17:38:03 +0100

    ajout du saut

--- Assets/script/PlayerMovement.cs
+++ Assets/script/PlayerMovement.cs
@@ -3,6 +3,11 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed;
+    public float jumpForce;
+    public bool isJumping;
+    public bool isGrounded;
+    public Transform GroundCheckLeft;
+    public Transform GroundCheckRight;
     public Rigidbody2D rb;
     private Vector3 velocity = Vector3.zero;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -14,12 +19,23 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        isGrounded = Physics2D.OverlapArea(GroundCheckLeft.position, GroundCheckRight.position);
         float horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.fixedDeltaTime;
+        if (Input.GetButton("Jump") && isGrounded)
+        {
+            isJumping = true;   
+
+        }
         MovePlayer(horizontalMovement);
     }
     void MovePlayer(float _horizontalMovement)
     {
         Vector3 targetVelocity = new Vector2(_horizontalMovement, rb.linearVelocity.y);
         rb.linearVelocity = Vector3.SmoothDamp(rb.linearVelocity, targetVelocity, ref velocity, .05f);
+        if(isJumping)
+        {
+            rb.AddForce(new Vector2(0f, jumpForce));
+            isJumping = false;
+        }
     }
 }
